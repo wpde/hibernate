5e6fac3a691bbc41ff27d839e93d02f7c3f9a5a4
cofe <cofe@LAPTOP-TLLB6TTE>
2018-12-10 20:53:26 +0800

    持久化对象状态和缓存

--- src/demo2/HibernateDemo2.java
+++ src/demo2/HibernateDemo2.java
@@ -20,6 +20,7 @@ public class HibernateDemo2 {
 		System.out.println(customer);
 	}
 	
+//	主键生成策略
 	@Test
 	public void test2() {
 		Session session = new Configuration().configure().buildSessionFactory().openSession();
@@ -30,5 +31,29 @@ public class HibernateDemo2 {
 		session.save(customer);
 		ts.commit();
 	}
+//	对象状态
+	@Test
+	public void test3() {
+		Session session = new Configuration().configure().buildSessionFactory().openSession();
+		Transaction ts = session.beginTransaction();
+		Customer customer = session.get(Customer.class, 1l);
+		customer.setCust_level("1");
+		ts.commit();
+		session.close();
+	}
 	
+//	一级缓存
+	@Test
+	public void test4() {
+		Session session = new Configuration().configure().buildSessionFactory().openSession();
+		Customer customer = session.get(Customer.class, 1l);
+//		clear()：清空一级缓存。
+//		evict()：清空一级缓存中指定的某个对象。
+//		refresh()：重新查询数据库，用数据库中的信息来更新一级缓存与快照区。
+		Customer customer2 = session.get(Customer.class, 1l);
+		System.out.println(customer);
+		System.out.println(customer2);
+		System.out.println(customer2==customer);
+		
+	}
 }
